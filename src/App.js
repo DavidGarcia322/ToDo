e19c0b0dd9fb388d546d4891b828c22398465beb
DavidGarcia322 <58706418+DavidGarcia322@users.noreply.github.com>
2020-03-09 15:59:16 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+  
+import React from "react";
+import "./App.css";
+
+import Todos from "./components/Todos";
+
+function App() {
+  return (
+    <div className="container-flex">
+      <Todos></Todos>
+    </div>
+  );
+}
+
+export default App;
